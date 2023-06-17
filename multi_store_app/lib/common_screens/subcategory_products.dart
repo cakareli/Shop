0dd0fb15d735a587801149387c523c2a04f23c6f
Strahinja Cvijanovic <strahinja.cvijanovic@rationaletech.com>
2023-06-17 14:00:16 +0200

    minor changes

--- multi_store_app/lib/common_screens/subcategory_products.dart
+++ multi_store_app/lib/common_screens/subcategory_products.dart
@@ -5,11 +5,9 @@ import '../widgets/appbar_widgets.dart';
 class SubcategoryProducts extends StatelessWidget {
   final String mainCategoryName;
   final String subcategoryName;
-  const SubcategoryProducts({
-    required this.mainCategoryName,
-    required this.subcategoryName,
-    Key? key,
-  }) : super(key: key);
+  const SubcategoryProducts(
+      {required this.mainCategoryName, required this.subcategoryName, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
